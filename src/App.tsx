4bfec0f82335132f36e8805738496f46d3bd0310
Yan Fernandes <yanfernandes404@gmail.com>
2023-10-14 00:17:35 -0300

    refactor: add alert on the project

--- src/App.tsx
+++ src/App.tsx
@@ -78,14 +78,16 @@ function App() {
   // If no provider was set, that means metamask is not active or not installed
   if (!provider) {
     return (
-      <div>
-        <Alert>
-          <RocketIcon className="h-4 w-4" />
-          <AlertTitle>Metamask not installed</AlertTitle>
-          <AlertDescription>
-            Please install Metamask to use this application.
-          </AlertDescription>
-        </Alert>
+      <div className="flex justify-center items-center h-screen">
+        <div className="bg-white rounded-lg p-2 shadow-lg">
+          <Alert>
+            <RocketIcon className="h-4 w-4" />
+            <AlertTitle>Metamask not installed</AlertTitle>
+            <AlertDescription>
+              Please install Metamask to use this application.
+            </AlertDescription>
+          </Alert>
+        </div>
       </div>
     );
   }
